014b6d480fccc2ccd652434dcf4a6b9686f346ec
vasanth2929 <vasanth10098@gmail.com>
2021-10-01 11:46:16 +0530

    first commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+
+  import { fly } from "svelte/transition";
+  import Container from "./Container.svelte";
+  import Profile from "./Profile.svelte";
+  let showBlock: boolean = false;
+  let timer = null;
+
+  onMount(() => {
+    timer = setTimeout(() => {
+      showBlock = true;
+    }, 1200);
+  });
+  onDestroy(() => {
+    clearTimeout(timer);
+  });
+</script>
+
+<main class="main">
+  {#if showBlock}
+    <div class="name" transition:fly={{ x: 0, y: -100 }}>
+      <h3>VASANTH</h3>
+      <p>FRONTEND DEVELOPER</p>
+    </div>
+  {/if}
+</main>
+
+<Container>
+  <Profile />
+</Container>
+
+<style>
+  .main {
+    background-color: rgb(131, 131, 131);
+    height: 120px;
+  }
+  .name {
+    background-color: white;
+    position: relative;
+    top: 50px;
+    width: 600px;
+    margin: auto;
+    padding: 20px;
+    text-align: center;
+    border: 2px solid var(--primaryColor);
+    border-radius: 2px;
+  }
+  .name h3 {
+    font-size: 60px;
+    letter-spacing: 5px;
+  }
+  .name p {
+    font-size: 16px;
+    letter-spacing: 2px;
+  }
+</style>
---
+++ src/components/Profile.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+
+  import { fly } from "svelte/transition";
+  let timer = null;
+  let showBlock = false;
+  onMount(() => {
+    timer = setTimeout(() => {
+      showBlock = true;
+    }, 1200);
+  });
+  onDestroy(() => {
+    clearTimeout(timer);
+  });
+</script>
+
+{#if showBlock}
+  <section class="profile-section" transition:fly={{ duration: 1000, x: 500 }}>
+    <div class="wrapper">
+      <div class="line" />
+      <h5>Profile</h5>
+      <div class="line" />
+    </div>
+    <div class="description">
+      I am passionate about Frontend Technologies & I enjoy learning new things.
+    </div>
+  </section>
+{/if}
+
+<style>
+  .profile-section {
+    text-align: center;
+  }
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .wrapper h5 {
+    font-size: 25px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    margin: 0px 20px;
+    color: var(--textColor);
+  }
+  .wrapper .line {
+    width: 70px;
+    height: 2px;
+    background: var(--textColor);
+  }
+  .description {
+    margin-top: 20px;
+  }
+</style>
