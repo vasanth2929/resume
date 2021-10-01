d129153e561a24e42eb0b3b4ee27d5bdfd056edd
vasanth2929 <vasanth10098@gmail.com>
2021-10-01 13:13:26 +0530

    update: transition component delay prop changes

--- src/components/shared/Transition.svelte
+++ src/components/shared/Transition.svelte
@@ -3,23 +3,26 @@
   import { fly } from "svelte/transition";
   export let x: number = 0;
   export let y: number = 0;
-  export let delay: number = 0;
+  export let delay: number = 1200;
   export let duration: number = 1000;
   export let opacity: number = 1;
   let showBlock: boolean = false;
+  export let height: string = "auto";
   let timer = null;
   onMount(() => {
     timer = setTimeout(() => {
       showBlock = true;
-    }, 1200);
+    }, delay);
   });
   onDestroy(() => {
     clearTimeout(timer);
   });
 </script>
 
-{#if showBlock}
-  <div class="name" transition:fly={{ x, y, delay, duration, opacity }}>
-    <slot />
-  </div>
-{/if}
+<div style={`min-height:${height}`}>
+  {#if showBlock}
+    <div class="name" transition:fly={{ x, y, duration, opacity }}>
+      <slot />
+    </div>
+  {/if}
+</div>
